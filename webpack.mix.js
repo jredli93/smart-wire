9005960fb9641e5ad4b1c35065810492bbfcc4d4
jredli993 <redlijovan@gmail.com>
2021-02-11 13:16:39 +0100

    webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/main.js', 'public/js')
-    .sass('resources/sass/main.scss', 'public/css')
-    .options({
-      processCssUrls: false
-   });;
+    .sass('resources/sass/main.scss', 'public/css');
