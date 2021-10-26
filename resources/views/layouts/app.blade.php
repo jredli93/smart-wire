3470e0478ce4feb0b24430b65807306aa0f66e58
Jovan Redli <>
2021-10-26 21:56:01 +0200

    meta title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Dado.ch') }}</title>
+    <title>Smart Wire</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
